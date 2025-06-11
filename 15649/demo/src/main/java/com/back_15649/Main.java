303fdb627e6b9cf592fa3a5d13d04e0fcdc9d77a
MillionKiwi <leehh41563636@gmail.com>
2025-06-12 08:56:03 +0900

    15649 solved

--- 15649/demo/src/main/java/com/back_15649/Main.java
+++ 15649/demo/src/main/java/com/back_15649/Main.java
@@ -1,7 +1,42 @@
 package com.back_15649;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N];
+        arr = new int[M];
+
+        dfs(0);
+    }
+
+    static void dfs(int depth) {
+        //깊이를 만족하면 출력
+        if(depth == M) {
+            for(int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        //방문하지 않은 숫자를 방문하고 깊이를 증가
+        for(int i = 0; i < N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
     }
 }
