4279965a8ef9dd946847b25462d1504c9c675fbf
MillionKiwi <leehh41563636@gmail.com>
2025-06-17 15:07:40 +0900

    11047 solved

--- 11047/demo/src/main/java/com/baek_11047/Main.java
+++ 11047/demo/src/main/java/com/baek_11047/Main.java
@@ -1,7 +1,31 @@
 package com.baek_11047;
+import java.util.*;
+import java.io.*;
+
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] coins = new int[N];
+
+        for(int i = 0; i < N; i++){
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+
+        for(int i = N - 1; i >= 0; i--){
+            int value = K / coins[i];
+            if(value > 0){
+                count += value;
+                K -= value * coins[i];
+            }
+        }
+
+        System.out.println(count);
     }
 }
