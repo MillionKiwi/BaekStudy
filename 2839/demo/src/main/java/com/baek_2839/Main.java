a19fe917c98dcf5f4ccd03db533625b69fcafa73
MillionKiwi <leehh41563636@gmail.com>
2025-06-24 16:18:19 +0900

    2839 solved with dp

--- 2839/demo/src/main/java/com/baek_2839/Main.java
+++ 2839/demo/src/main/java/com/baek_2839/Main.java
@@ -2,7 +2,11 @@ package com.baek_2839;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.*;
 
+/// 그리디로 solved
+/* 
 public class Main {
     public static void main(String[] args) throws IOException{
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,5 +27,42 @@ public class Main {
        } 
 
        System.out.println(min_bag);
+    }
+}
+*/
+
+
+/// DP로 풀어보기
+public class Main{
+    static int BigNum = 5001;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        
+        int[] dp = new int[N+1];
+        Arrays.fill(dp,BigNum);
+
+        dp[0] = 0;
+
+        for(int i = 3; i <= N; i++){
+            if(dp[i - 3] != BigNum){
+                dp[i] = Math.min(dp[i],dp[i-3]+1);
+            }
+        }
+
+
+        for(int i = 5; i <= N; i++){
+            if(dp[i - 5] != BigNum){
+                dp[i] = Math.min(dp[i],dp[i-5]+1);
+            }
+        }
+        
+        if (dp[N] == BigNum) { 
+            System.out.println(-1);
+        } else { 
+            System.out.println(dp[N]);
+        }
+
     }
 }
