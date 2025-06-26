def4782c45592196e5781d3165f0dc7f9643e540
MillionKiwi <leehh41563636@gmail.com>
2025-06-26 15:29:00 +0900

    1932 solved

--- 1932/demo/src/main/java/com/baek_1932/Main.java
+++ 1932/demo/src/main/java/com/baek_1932/Main.java
@@ -1,7 +1,30 @@
 package com.baek_1932;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt((br.readLine()));
+
+        int[][] triangle = new int[n][n];
+
+    
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j <= i; j++){
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                triangle[i][j] += Math.max(triangle[i+1][j],triangle[i+1][j+1]);
+            }
+        }
+
+        System.out.println(triangle[0][0]);
+
     }
 }
