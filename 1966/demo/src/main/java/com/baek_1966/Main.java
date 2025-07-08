a8c94a55fce7d51fbfc0fa725f045230b6ab38d2
MillionKiwi <leehh41563636@gmail.com>
2025-07-08 16:36:52 +0900

    1966 solved

--- 1966/demo/src/main/java/com/baek_1966/Main.java
+++ 1966/demo/src/main/java/com/baek_1966/Main.java
@@ -1,7 +1,67 @@
 package com.baek_1966;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static class Paper{
+        public int priority;
+        public int index;
+
+        public Paper(int priority, int index){
+            this.priority = priority;
+            this.index = index;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t= 0; t< T; t++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            LinkedList<Paper> queue = new LinkedList<>();
+
+            st = new StringTokenizer(br.readLine());
+
+            for(int i = 0; i < N; i++){
+                queue.add(new Paper(Integer.parseInt(st.nextToken()), i));
+            }
+
+            int printCount = 0;
+
+            //queue가 빌 때까지 출력
+            while(!queue.isEmpty()){
+                Paper nowPaper = queue.poll();
+                boolean isPrintable = true;
+
+                for(Paper p : queue){
+                    if(nowPaper.priority < p.priority){
+                        isPrintable = false;
+                        break;
+                    }
+                }
+
+                if(isPrintable){
+                    printCount++;   //인쇄 확정
+                    if(nowPaper.index == M) 
+                        break;
+                }
+                else
+                    queue.add(nowPaper);
+            }
+
+            System.out.println(printCount);
+            
+        }
     }
 }
