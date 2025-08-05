30388bfcd9fdd719aefaa3f4b521ec1bf2405dd9
MillionKiwi <leehh41563636@gmail.com>
2025-08-05 17:47:06 +0900

    11726 solved

--- w8_1st/w8_1/src/Baek11726.java
+++ w8_1st/w8_1/src/Baek11726.java
@@ -1,5 +1,21 @@
+import java.io.*;
+import java.util.*;
+
 public class Baek11726 {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int MOD = 10007;
+        int[] dp = new int[Math.max(3, n + 1)]; // 최소 크기 3으로 설정
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[n]);
     }
 }
