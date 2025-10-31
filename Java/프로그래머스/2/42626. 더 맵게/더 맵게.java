af6eaee4b04dc529280a0e1fbc8ae07e0096c6d7
LEE HYUNHO <127092247+MillionKiwi@users.noreply.github.com>
2025-10-31 09:49:06 +0900

    [level 2] Title: 더 맵게, Time: 1588.31 ms, Memory: 128 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i = 0; i < scoville.length; i++){
+            pq.add(scoville[i]);
+        }
+        
+        
+        while(pq.peek() < K){
+            if(pq.size() < 2) return -1;
+            
+            int firstFood = pq.poll();
+            int secondFood = pq.poll();
+            int newFood = firstFood + 2*secondFood;
+            
+            pq.add(newFood);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
